02db57e63e5706902ac3c7581807820c45468b1a
t.mohatat <tinamohtat@gmail.com>
2017-05-31 12:20:45 +0430

    helloo222

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -4,6 +4,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("helloooooooooo");
-        System.out.println("milad");
+        System.out.println("tinaaaaa");
+        System.out.println("miladhasaaaanlooo");
     }
 }
