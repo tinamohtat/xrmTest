55667ab6f1531c98b9a362681b6db00fb0ceb1c0
milad <m.hajihasanlo@etick.ir>
2017-05-31 12:19:42 +0430

    salam2

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -1,9 +1,8 @@
-/**
- * Created by pc on 5/31/17.
- */
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("helloooooooooo");
         System.out.println("milad");
+        System.out.println("salam");
     }
 }
